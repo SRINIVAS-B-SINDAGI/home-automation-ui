ffc33eeece57bafcf29fdc0fcb9af98e022662cf
SRINIVAS-B-SINDAGI <srinivas.sindagi@hebbalelabs.com>
2020-04-05 13:48:00 +0530

    * fix alignment issue * bug-fix multiple device being added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import "./App.css";
 import {
   AppBar,
@@ -17,11 +17,11 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import TextField from "@material-ui/core/TextField";
 import Snackbar from "@material-ui/core/Snackbar";
 import axios from "axios";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 interface Device {
   smart_device_iD: string;
   smart_device_name: string;
@@ -33,7 +33,7 @@ const useStyles = makeStyles({
     flexGrow: 1,
   },
   card: {
-    width: 375,
+    width: 500,
   },
   onButton: {
     position: "relative" as "relative",
@@ -53,24 +53,33 @@ const useStyles = makeStyles({
     cursor: "pointer" as "pointer",
   },
   addDevice: {
-    marginLeft: "45%",
+    float: "right",
+    marginRight: "86px",
+    marginTop: "-8px",
+    cursor: "pointer",
+  },
+  snackBarMessage: {
+    background: "#4caf50",
+    color: "white",
+  },
+  customDivider: {
+    width: "1098px",
   },
 });
 
 const App = () => {
+  const classes = useStyles();
   const [listDevices, setListDevices] = React.useState([]);
+  const [snackBar, setSnackBar] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const [deviceName, setDeviceName] = React.useState("");
 
   useEffect(() => {
-    // Update the document title using the browser API
     axios.get(`http://localhost:5000/listAllDevices`).then((res) => {
       setListDevices(res.data);
       console.log(res.data);
     });
   }, []);
-
-  const [snackBar, setSnackBar] = React.useState(false);
-  const [open, setOpen] = React.useState(false);
-  const [deviceName, setDeviceName] = React.useState("");
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -90,8 +99,6 @@ const App = () => {
       window.location.reload();
     }, 4000);
   };
-
-  const classes = useStyles();
 
   const deleteDevice = (deviceId: string) => {
     axios
@@ -117,128 +124,143 @@ const App = () => {
     window.location.reload();
   };
 
+  const handleDialogClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Fragment>
       <AppBar>
         <span className="navbar">Home automation</span>
       </AppBar>
-      <Container>
-        <div className="contentContainer">
-          <Grid container={true} spacing={6}>
-            {listDevices.map((getDevice: Device) => (
-              <Grid item={true} lg={4}>
-                <Card className={classes.card}>
-                  <CardHeader
-                    title={`${getDevice.smart_device_name} ${
-                      getDevice.smart_device_status
-                        ? "| current status: on"
-                        : "| current status: off"
-                    }`}
-                  />
-                  <Divider />
-                  <CardContent>
-                    <EmojiObjectsIcon
-                      color={
-                        getDevice.smart_device_status ? "primary" : "inherit"
-                      }
-                      fontSize="large"
-                    />
-                    <div className="card-button__block">
-                      {getDevice.smart_device_status ? (
-                        <Button
-                          variant="contained"
-                          color="secondary"
-                          className={classes.onButton}
-                          onClick={() =>
-                            switchOff(
-                              getDevice.smart_device_iD,
-                              getDevice.smart_device_status
-                            )
-                          }
-                        >
-                          Off
-                        </Button>
-                      ) : (
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          className={classes.ofButton}
-                          onClick={() =>
-                            switchOff(
-                              getDevice.smart_device_iD,
-                              getDevice.smart_device_status
-                            )
-                          }
-                        >
-                          On
-                        </Button>
-                      )}
-                    </div>
-                    <DeleteOutlineIcon
-                      color="primary"
-                      className={classes.deleteButton}
-                      fontSize="large"
-                      onClick={() => deleteDevice(getDevice.smart_device_iD)}
-                    />
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-          {listDevices.length <= 0 && (
-            <div className="loading-state__block">
-              <CircularProgress />
-            </div>
-          )}
-          <hr />
-          <div>
-            <Button
-              variant="contained"
-              color="primary"
-              className={classes.addDevice}
-              onClick={handleClickOpen}
-            >
-              Add device
-            </Button>
-            <Dialog
-              open={open}
-              onClose={handleClose}
-              aria-labelledby="form-dialog-title"
-            >
-              <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-              <DialogContent>
-                <DialogContentText>
-                  Please enter device name below:
-                </DialogContentText>
-                <TextField
-                  autoFocus
-                  margin="dense"
-                  id="deviceName"
-                  label="device Name"
-                  type="text"
-                  fullWidth
-                  onChange={(event) => setDeviceName(event.target.value)}
-                />
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={handleClose} color="primary">
-                  submit
-                </Button>
-              </DialogActions>
-            </Dialog>
+      <Container maxWidth="lg">
+        <Container>
+          <div className="device-title__section">
+            <span>List of Devices</span>
+            <span>
+              <AddCircleOutlineIcon
+                color="primary"
+                className={classes.addDevice}
+                onClick={handleClickOpen}
+              />
+              {/* <span>Add Device</span> */}
+            </span>
           </div>
-          <Snackbar
-            anchorOrigin={{
-              vertical: "bottom",
-              horizontal: "center",
-            }}
-            open={snackBar}
-            autoHideDuration={2000}
-            className="snack-bar"
-            onClose={handleClose}
-            message={deviceName}
-          />
-        </div>
+          <Divider className={classes.customDivider} />
+          <div className="contentContainer">
+            <Grid container={true} spacing={2}>
+              {listDevices.map((getDevice: Device) => (
+                <Grid item={true} lg={6}>
+                  <Card className={classes.card}>
+                    <CardHeader
+                      title={`${getDevice.smart_device_name} ${
+                        getDevice.smart_device_status
+                          ? "| current status: on"
+                          : "| current status: off"
+                      }`}
+                    />
+                    <Divider />
+                    <CardContent>
+                      <EmojiObjectsIcon
+                        color={
+                          getDevice.smart_device_status ? "primary" : "inherit"
+                        }
+                        fontSize="large"
+                      />
+                      <div className="card-button__block">
+                        {getDevice.smart_device_status ? (
+                          <Button
+                            variant="contained"
+                            color="secondary"
+                            className={classes.onButton}
+                            onClick={() =>
+                              switchOff(
+                                getDevice.smart_device_iD,
+                                getDevice.smart_device_status
+                              )
+                            }
+                          >
+                            Off
+                          </Button>
+                        ) : (
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            className={classes.ofButton}
+                            onClick={() =>
+                              switchOff(
+                                getDevice.smart_device_iD,
+                                getDevice.smart_device_status
+                              )
+                            }
+                          >
+                            On
+                          </Button>
+                        )}
+                      </div>
+                      <DeleteOutlineIcon
+                        color="primary"
+                        className={classes.deleteButton}
+                        fontSize="large"
+                        onClick={() => deleteDevice(getDevice.smart_device_iD)}
+                      />
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+            {listDevices.length <= 0 && (
+              <div className="loading-state__block">
+                <CircularProgress />
+              </div>
+            )}
+            <div>
+              <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="form-dialog-title"
+              >
+                <DialogContent>
+                  <DialogContentText>
+                    Please enter device name :
+                  </DialogContentText>
+                  <TextField
+                    autoFocus
+                    margin="dense"
+                    id="deviceName"
+                    label="Device Name"
+                    type="text"
+                    fullWidth
+                    onChange={(event) => setDeviceName(event.target.value)}
+                  />
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={handleDialogClose} color="primary">
+                    cancel
+                  </Button>
+                  <Button onClick={handleClose} color="primary">
+                    ok
+                  </Button>
+                </DialogActions>
+              </Dialog>
+            </div>
+            <Snackbar
+              ContentProps={{
+                classes: {
+                  root: classes.snackBarMessage,
+                },
+              }}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              open={snackBar}
+              autoHideDuration={2000}
+              className="snack-bar"
+              message={`Device name: [${deviceName}] has been added`}
+            />
+          </div>
+        </Container>
       </Container>
     </Fragment>
   );
